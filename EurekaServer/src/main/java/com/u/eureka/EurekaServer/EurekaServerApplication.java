af6fbbcf4f21f8cdd1df21b30757b77e7bfdab31
nragh <nragh@LAPTOP-AEOHVJQ7>
2019-06-09 20:39:27 +0530

    Updated by rmareddy

--- EurekaServer/src/main/java/com/u/eureka/EurekaServer/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/u/eureka/EurekaServer/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.u.eureka.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RefreshScope
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
